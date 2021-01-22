ec21557f85237404084617b156de61675ec51d6d
Dan <dan@dantechmedia.com>
2021-01-21 16:52:04 -0800

    added animation to menu burger & toggle to menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$(document).ready(function () {
-  $(".menu-button").click(function () {
-    $("nav ul").toggleClass("active");
-    $(".menu-button i").toggleClass("active");
-  });
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
+const menuIcon = document.querySelector(".menu-burger");
+const navbar = document.querySelector(".nav-list");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("change");
 });
