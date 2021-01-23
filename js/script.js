9e581091a9b957c080fdf3d69735a32df7a2da03
Dan <dan@dantechmedia.com>
2021-01-22 17:57:38 -0800

    fixed menu .toggle feature and worked on content

--- js/script.js
+++ js/script.js
@@ -10,9 +10,32 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-const menuIcon = document.querySelector(".menu-burger");
+const menuIcon = document.querySelector(".menu-btn");
 const navbar = document.querySelector(".nav-list");
 
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+// Sticky Navbar
+
+// Smooth Scroll
+const body = document.body,
+  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+  height = scrollWrap.getBoundingClientRect().height - 1,
+  speed = 0.04;
+
+var offset = 0;
+
+body.style.height = Math.floor(height) + "px";
+
+function smoothScroll() {
+  offset += (window.pageYOffset - offset) * speed;
+
+  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+  scrollWrap.style.transform = scroll;
+
+  callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
