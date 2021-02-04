0accf0bc2d1f9880a0c5b5b6955ce9d5506d49d2
Dan <dan@dantechmedia.com>
2021-02-04 14:16:54 -0800

    fixed bug with margin on glass-container

--- js/script.js
+++ js/script.js
@@ -26,25 +26,25 @@ window.addEventListener("scroll", function () {
 });
 
 // Smooth Scroll
-const body = document.body,
-  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-  height = scrollWrap.getBoundingClientRect().height - 1,
-  speed = 0.04;
+// const body = document.body,
+//   scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+//   height = scrollWrap.getBoundingClientRect().height - 1,
+//   speed = 0.04;
 
-var offset = 0;
+// var offset = 0;
 
-body.style.height = Math.floor(height) + "px";
+// body.style.height = Math.floor(height) + "px";
 
-function smoothScroll() {
-  offset += (window.pageYOffset - offset) * speed;
+// function smoothScroll() {
+//   offset += (window.pageYOffset - offset) * speed;
 
-  var scroll = "translateY(-" + offset + "px) translateZ(0)";
-  scrollWrap.style.transform = scroll;
+//   var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//   scrollWrap.style.transform = scroll;
 
-  callScroll = requestAnimationFrame(smoothScroll);
-}
+//   callScroll = requestAnimationFrame(smoothScroll);
+// }
 
-smoothScroll();
+// smoothScroll();
 
 // TEST
 // const body = document.body,
