d25ab28cb142e6b5c742e9b28a0fd00e17a9b94f
Dan <dan@dantechmedia.com>
2021-02-04 13:38:55 -0800

    fixed bug with projects-container not in center

--- js/script.js
+++ js/script.js
@@ -26,12 +26,34 @@ window.addEventListener("scroll", function () {
 });
 
 // Smooth Scroll
-// const body = document.getElementsByClassName("content-wrapper")[0],
+const body = document.body,
+  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+  height = scrollWrap.getBoundingClientRect().height - 1,
+  speed = 0.04;
+
+var offset = 0;
+
+body.style.height = Math.floor(height) + "px";
+
+function smoothScroll() {
+  offset += (window.pageYOffset - offset) * speed;
+
+  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+  scrollWrap.style.transform = scroll;
+
+  callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
+
+// TEST
+// const body = document.body,
 //   scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
 //   height = scrollWrap.getBoundingClientRect().height - 1,
 //   speed = 0.04;
 
 // var offset = 0;
+
 // body.style.height = Math.floor(height) + "px";
 
 // function smoothScroll() {
