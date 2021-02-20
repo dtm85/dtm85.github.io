09df874a700714a595ae91586240ad5765151d0f
Dan <dan@dantechmedia.com>
2021-02-20 14:06:37 -0800

    working on hamburger menu and custom scroll bar

--- projects/tea-station/app.js
+++ projects/tea-station/app.js
@@ -1,15 +1,29 @@
 // setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+// const navBtn = document.getElementById("nav-btn");
+// const navbar = document.getElementById("navbar");
+// const navClose = document.getElementById("nav-close");
 // show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
+// navBtn.addEventListener("click", () => {
+//   navbar.classList.add("showNav");
+// });
 // close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+// navClose.addEventListener("click", () => {
+//   navbar.classList.remove("showNav");
+// });
+
+// Hamburger Menu Animation
+const menuBtn = document.querySelector(".nav-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
 });
+
 //setup date
 // const date = (document.getElementById(
 //   "date"
