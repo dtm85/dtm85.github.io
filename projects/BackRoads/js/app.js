4cbf25b09886f951ec24d3d91173a0aba7620e3d
Dan <dan@dantechmedia.com>
2021-03-04 06:29:40 -0800

    added smooth scroll for navigation on click

--- projects/BackRoads/js/app.js
+++ projects/BackRoads/js/app.js
@@ -1,4 +1,3 @@
-// ********** nav toggle ************
 // Hamburger Menu Animation
 const menuBtn = document.querySelector(".nav-btn");
 let menuOpen = false;
@@ -12,38 +11,68 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-const menuIcon = document.querySelector(".nav-btn");
-const navbar = document.querySelector(".nav-links");
+// ********** nav toggle ************
+const navBtn = document.getElementById("nav-btn");
+const links = document.getElementById("nav-links");
 
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("show-links");
+// Add Event Listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
 });
 
 // ********** smooth scroll ************
-// select links
-const scrollLinks = document.querySelectorAll("scroll-link");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
-    // prevent default
     e.preventDefault();
+
     links.classList.remove("show-links");
+    // Remove the open class for nav-btn to change menu animation from X to Menu
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+    }
 
     const id = e.target.getAttribute("href").slice(1);
     const element = document.getElementById(id);
-    //
+
     let position = element.offsetTop - 62;
 
+    // Window scrollTo
     window.scrollTo({
       left: 0,
-      // top: element.offsetTop,
       top: position,
       behavior: "smooth",
     });
   });
 });
 
-// ********** Navbar Styles on Scroll Y ************
-// Navbar Styles applied on Scroll Y
+// ********** Smooth Scroll on Footer Links ************
+const smoothScroll = document.querySelectorAll(".smooth-scroll");
+
+smoothScroll.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    // Window scrollTo
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
+
+// ********** Navbar Styles on ScrollY ************
 window.addEventListener("scroll", function () {
   let header = document.querySelector(".navbar");
   let windowPosition = window.scrollY > 0;
