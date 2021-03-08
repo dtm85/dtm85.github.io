0907c7a60986b522d66d1bccf947f0914192e9c1
Dan <dan@dantechmedia.com>
2021-03-08 05:26:56 -0800

    removed extra text from svg's & added menu button

--- projects/Portfolio/js/app.js
+++ projects/Portfolio/js/app.js
@@ -1,22 +1,122 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
+// ************** OLD **************
+
+// const navbar = document.querySelector("#nav");
+// const navBtn = document.querySelector("#nav-btn");
+// const closeBtn = document.querySelector("#close-btn");
+// const sidebar = document.querySelector("#sidebar");
+// const date = document.querySelector("#date");
+// // add fixed class to navbar
+// window.addEventListener("scroll", function () {
+//   if (window.pageYOffset > 80) {
+//     navbar.classList.add("navbar-fixed");
+//   } else {
+//     navbar.classList.remove("navbar-fixed");
+//   }
+// });
+// // show sidebar
+// navBtn.addEventListener("click", function () {
+//   sidebar.classList.add("show-sidebar");
+// });
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
+// // set year
+// date.innerHTML = new Date().getFullYear();
+
+// ************** NEW **************
+// Hamburger Menu Animation
+const menuBtn = document.querySelector(".nav-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
   } else {
-    navbar.classList.remove("navbar-fixed");
+    menuBtn.classList.remove("open");
+    menuOpen = false;
   }
 });
-// show sidebar
-navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
+
+// ********** nav toggle ************
+const navBtn = document.getElementById("nav-btn");
+const links = document.getElementById("nav-links");
+
+// Add Event Listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
 });
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+
+// ********** smooth scroll ************
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    links.classList.remove("show-links");
+    // Remove the open class for nav-btn to change menu animation from X to Menu
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+    }
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    // Window scrollTo
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
 });
-// set year
-date.innerHTML = new Date().getFullYear();
+
+// ********** Smooth Scroll on Footer Links ************
+const smoothScroll = document.querySelectorAll(".smooth-scroll");
+
+smoothScroll.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    // Window scrollTo
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
+
+// ********** Navbar Styles on ScrollY ************
+window.addEventListener("scroll", function () {
+  let header = document.querySelector(".navbar");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowPosition);
+});
+
+// Navbar Hide on Top - Shows on scroll down
+const nav = document.querySelector(".navbar");
+let prevScrollpos = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    nav.classList.add("hide");
+  } else {
+    nav.classList.remove("hide");
+  }
+
+  prevScrollpos = currentScrollPos;
+});
