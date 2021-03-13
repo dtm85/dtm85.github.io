79e077a736e271361d661be43464706c38e53540
Dan <dan@dantechmedia.com>
2021-03-12 17:03:43 -0800

    completed connect & skills and started on timeline

--- projects/Portfolio/js/app.js
+++ projects/Portfolio/js/app.js
@@ -1,29 +1,3 @@
-// ************** OLD **************
-
-// const navbar = document.querySelector("#nav");
-// const navBtn = document.querySelector("#nav-btn");
-// const closeBtn = document.querySelector("#close-btn");
-// const sidebar = document.querySelector("#sidebar");
-// const date = document.querySelector("#date");
-// // add fixed class to navbar
-// window.addEventListener("scroll", function () {
-//   if (window.pageYOffset > 80) {
-//     navbar.classList.add("navbar-fixed");
-//   } else {
-//     navbar.classList.remove("navbar-fixed");
-//   }
-// });
-// // show sidebar
-// navBtn.addEventListener("click", function () {
-//   sidebar.classList.add("show-sidebar");
-// });
-// closeBtn.addEventListener("click", function () {
-//   sidebar.classList.remove("show-sidebar");
-// });
-// // set year
-// date.innerHTML = new Date().getFullYear();
-
-// ************** NEW **************
 // Hamburger Menu Animation
 const menuBtn = document.querySelector(".nav-btn");
 let menuOpen = false;
@@ -104,20 +78,3 @@ window.addEventListener("scroll", function () {
   let windowPosition = window.scrollY > 0;
   nav.classList.toggle("navbar-fixed", windowPosition);
 });
-
-// ********** Do I really need this one??? ************
-// Navbar Hide on Top - Shows on scroll down
-// const nav = document.querySelector(".navbar");
-// let prevScrollpos = window.pageYOffset;
-
-// window.addEventListener("scroll", () => {
-//   let currentScrollPos = window.pageYOffset;
-
-//   if (prevScrollpos > currentScrollPos) {
-//     nav.classList.add("hide");
-//   } else {
-//     nav.classList.remove("hide");
-//   }
-
-//   prevScrollpos = currentScrollPos;
-// });
