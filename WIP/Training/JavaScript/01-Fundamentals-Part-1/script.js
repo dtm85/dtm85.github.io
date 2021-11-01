950e8136fb89511a725407a21aed714497761cf8
Dan Dumitru <dan@dantechmedia.com>
2021-11-01 16:40:22 -0700

    learning about truthy values, equality & difference operators

--- WIP/Training/JavaScript/01-Fundamentals-Part-1/script.js
+++ WIP/Training/JavaScript/01-Fundamentals-Part-1/script.js
@@ -163,8 +163,47 @@ console.log("23" > "18");
 let n = "1" + 1; // Gives us "11" because the "1" is a string and the + operator converts the 1 to a string also.
 n = n - 1;
 console.log(n);
-*/
 
 // 5 falsy values: 0, " ", undefined, null, NaN
-// TEST
-// TEST #2
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Dan"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = "0";
+if (height) {
+  console.log("YAY! Height is defined!");
+} else {
+  console.log("Height is UNDEFINED!");
+}
+
+const age = "18";
+if (age === 18) console.log("You just became and adult! (STRICT)");
+
+if (age == 18) console.log("You just became and adult! (LOOSE)");
+
+const favorite = Number(prompt("What is your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 7) {
+  console.log("Cool! 7 is a very lucky number!");
+} else if (favorite === 8) {
+  console.log("8 is also a lucky number!");
+} else if (favorite === 9) {
+  console.log("9 is also a kind of lucky number!");
+} else {
+  console.log("Number is not 7, 8 or 9");
+}
+
+if (favorite !== 7) console.log("Why not 7?");
+*/
