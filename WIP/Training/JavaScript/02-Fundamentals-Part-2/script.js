8af5247af8bc974d8296f610160ce2b270ebb400
Dan Dumitru <dan@dantechmedia.com>
2021-11-04 07:17:40 -0700

    added course material for JavaScript Fundamentals 2

---
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive! 🚗");
+
+/////////////////////////////////////////////////
+// LEGEND FOR BETTER COMMENTS VSCode Extension //
+/////////////////////////////////////////////////
+
+//! Reserved words <-- (//! = This comment is for a Deprecated Method, do not use)
+//? Reserved words <-- (//? = This is a comment that asks a Question)
+//* Reserved words <-- (//* = This is an Important Comment [HIGHLIGHTED])
+//TODO Reserved words <-- (//// = This is a TODO comment)
+//// Reserved words<-- (//! = This is a Strikethrough Comment)
+// const interface = "Audio";
+// const private = "534";
+// const if = 23;
