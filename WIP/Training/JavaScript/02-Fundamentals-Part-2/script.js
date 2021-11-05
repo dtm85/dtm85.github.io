be0373f0bc9aad666bf1aa4042a89f62212d1507
Dan Dumitru <dan@dantechmedia.com>
2021-11-04 17:00:59 -0700

    learning about functions with template literals

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -18,3 +18,21 @@ if (hasDriversLicense) console.log("I can drive! 🚗");
 // const interface = "Audio";
 // const private = "534";
 // const if = 23;
+
+function logger() {
+  console.log("My name is Dan");
+}
+
+//* Calling / Running / Invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
