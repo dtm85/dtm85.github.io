389cbfe0a4edd28a04c4981854d913f50b7a59d9
Dan Dumitru <dan@dantechmedia.com>
2021-11-05 16:38:23 -0700

    learning about function declarations and function expressions

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -36,3 +37,19 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+//* Function decloration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+//* Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
