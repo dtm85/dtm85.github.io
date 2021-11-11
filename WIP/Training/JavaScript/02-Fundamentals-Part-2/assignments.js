da74d5bffb75f8c647ed8918d2fe39b527bca0f7
Dan Dumitru <dan@dantechmedia.com>
2021-11-10 16:16:14 -0800

    completed coding challenge #1 - functions

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/assignments.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/assignments.js
@@ -1,3 +1,4 @@
+/*
 // Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 // Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
@@ -9,16 +10,25 @@ const calcAverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcAverage(3, 4, 5));
 
 // Test Data #1
-const scoreDolphins = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreDolphins);
 
 const checkWinner = function (avgDolhins, avgKoalas) {
   if (avgDolhins >= 2 * avgKoalas) {
     console.log(`Dolphins win 🏆 (${avgDolhins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolhins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolhins})`);
   } else {
-    console.log(`Nobody Wins 😕`);
+    console.log(`No team wins...😕`);
   }
 };
 
+checkWinner(576, 111);
+
 // Test Data #2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
