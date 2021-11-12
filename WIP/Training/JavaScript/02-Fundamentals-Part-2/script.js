452951524d1419ad0ddcd1a7ba3db189f8d77557
Dan Dumitru <dan@dantechmedia.com>
2021-11-11 17:14:56 -0800

    learning about Arrays

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -103,3 +103,45 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1991, "Dan"));
 console.log(yearsUntilRetirement(1950, "Mike"));
 */
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "Teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//* Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
