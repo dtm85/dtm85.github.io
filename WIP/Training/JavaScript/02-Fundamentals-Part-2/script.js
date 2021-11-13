d3f8c31632f67f18a195b23feac9bf090cb3fb56
Dan Dumitru <dan@dantechmedia.com>
2021-11-12 16:47:55 -0800

    learning about Array (Methods)

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -102,7 +102,7 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, "Dan"));
 console.log(yearsUntilRetirement(1950, "Mike"));
-*/
+
 
 const friend1 = "Michael";
 const friend2 = "Steven";
@@ -145,3 +145,35 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
+*/
+
+const friends = ["Michael", "Steven", "Peter"];
+
+// Adds Elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Removes Elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log(`You have a friend called Steven`);
+}
