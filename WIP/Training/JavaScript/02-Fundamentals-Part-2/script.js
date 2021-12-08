c8cb9b88e16af1b206e0e323552c5daf7fc3ef7e
Dan <dan@dantechmedia.com>
2021-12-08 15:07:26 -0800

    learned about while loops

--- WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
+++ WIP/Training/JavaScript/02-Fundamentals-Part-2/script.js
@@ -352,7 +352,22 @@ for (let exercise = 1; exercise <= 3; exercise++) {
   }
 }
 
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting Weights Repetition ${rep} 🏋️‍♀️`);
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting Weights Repetition ${rep} 🏋️‍♀️`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting Weights Repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
 */
