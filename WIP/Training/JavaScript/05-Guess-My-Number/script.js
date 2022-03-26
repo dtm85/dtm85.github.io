08aa2cb2c6d42359862e6cc828969e6f882e46c2
Dan <dan@dantechmedia.com>
2022-03-26 08:55:15 -0700

    learning about click even listeners

--- WIP/Training/JavaScript/05-Guess-My-Number/script.js
+++ WIP/Training/JavaScript/05-Guess-My-Number/script.js
@@ -1,13 +1,23 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number!';
+  }
+});
