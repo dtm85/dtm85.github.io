bf1426edf5c1efcdd02ff81cf7e538ea9dde61eb
Dan Dumitru <dan@dantechmedia.com>
2022-05-15 17:07:11 -0700

    implemented storing and displaying the highscore value

--- WIP/Training/JavaScript/05-Guess-My-Number/script.js
+++ WIP/Training/JavaScript/05-Guess-My-Number/script.js
@@ -16,6 +16,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 // Click event on Again! button
 document.querySelector('.again').addEventListener('click', function () {
@@ -47,6 +48,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#10b147';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
