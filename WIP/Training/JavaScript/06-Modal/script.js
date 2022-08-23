d79da34296710967367cb65c47c13b1e0f0cfda3
Dan <dan@dantechmedia.com>
2022-08-23 16:58:05 -0700

    worked on manipulating classes using clickEvent handlers

--- WIP/Training/JavaScript/06-Modal/script.js
+++ WIP/Training/JavaScript/06-Modal/script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const OpenModal = function () {
+  console.log("Button Clicked!");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", OpenModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
