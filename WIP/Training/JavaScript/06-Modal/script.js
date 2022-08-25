1cbf5ca7605b0d516c63e74cb30a9607f4104b2d
Dan <dan@dantechmedia.com>
2022-08-24 17:06:35 -0700

    implemented functionality for modal close on Esc key press

--- WIP/Training/JavaScript/06-Modal/script.js
+++ WIP/Training/JavaScript/06-Modal/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 const OpenModal = function () {
-  console.log("Button Clicked!");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -21,3 +20,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
